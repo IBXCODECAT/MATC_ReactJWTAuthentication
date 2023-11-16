286936a5cc0cd65d985d299496b9e6f76e084b35
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-11-16 16:44:45 -0600

    Handle JWT Errors

--- Helpers/JWT.js
+++ Helpers/JWT.js
@@ -20,8 +20,7 @@ module.exports = {
         //Generate a secret key
         return new Promise((resolve, reject) => {
             JWT.sign(payload, secret, options, (err, token) => {
-                if(err)
-                {
+                if (err) {
                     console.error(err.message);
                     //reject(err); - SECURITY VULNERABLITY (ERROR LEAK), DO NOT USE
                     return reject(createError.InternalServerError());
@@ -32,21 +31,26 @@ module.exports = {
     },
 
     verifyAccessToken: (req, res, next) => {
-        if(!req.headers['authorization']) return next(createError.Unauthorized("Unauthorized"));
+        if (!req.headers['authorization']) return next(createError.Unauthorized("Unauthorized"));
 
         //Get the token from the header
         const authHeader = req.headers['authorization'];
         const beaerToken = authHeader.split(' ');
         const token = beaerToken[1];
 
-        JWT.verify(token, process.env.ACESS_TOKEN_SECRET, (err, payload) => { 
-            if(err)
-            {
+        JWT.verify(token, process.env.ACESS_TOKEN_SECRET, (err, payload) => {
+            if (err) {
                 console.error(err.message);
+
+                //By default this error would say something like "invalid signature" or "jwt malformed"
+                //This is a security vulnerablity because it gives away too much information about the error
+                //To fix this, we can use a package called "http-errors" to create a custom error
+                const message = err.name === 'JsonWebTokenError' ? 'Unauthorized' : err.message;
+
                 //401 = Unauthorized
-                return next(createError.Unauthorized("Unauthorized"));
+                return next(createError.Unauthorized(message));
             }
-            
+
             //Add the payload to the request
             req.payload = payload;
             next();
